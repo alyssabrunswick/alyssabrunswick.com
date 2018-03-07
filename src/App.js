9e456eeca1200acea17c62ac32ac6e145184b00e
Alyssa Brunswick <alyssabrunswick@Alyssas-iMac.local>
2018-03-06 21:52:02 -0600

    Initial setup of react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './styles/app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="header">
+          <h1 className="title">AB</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
