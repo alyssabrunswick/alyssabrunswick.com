ac695141ac2186ada428413cae701cb78b99a85b
Alyssa Brunswick <alyssa.brunswick@gmail.com>
2022-01-19 20:30:36 -0600

    initial commit of astro + svelte template with content replaced

---
+++ src/components/Footer.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let name = "Alyssa Brunswick";
+</script>
+
+<footer>
+  &copy; {new Date().getFullYear()}
+  {name}
+  <small>🌓 🌿 ✨</small>
+</footer>
+
+<style>
+  footer {
+    text-align: center;
+    padding: 8rem 2rem 4rem;
+  }
+
+  small {
+    display: block;
+    margin-top: 1rem;
+    color: var(--t-subdue);
+    font-size: var(--f-d2);
+    text-transform: uppercase;
+  }
+</style>
---
+++ src/pages/project/magic.md
@@ -0,0 +1,14 @@
+---
+layout: ../../layouts/project.astro
+title: Magic
+client: Self
+publishDate: 2020-03-02 00:00:00
+img: https://source.unsplash.com/random/1400×700/?magic&fit=crop&q=75
+description: |
+  Sample project page
+tags:
+  - design
+  - dev
+---
+
+Sample page (project?) one
