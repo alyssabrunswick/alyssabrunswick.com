f27c5ff8acdbdbbc092ee0df53b26e1ec5c6678e
Alyssa Brunswick <alyssa.brunswick@gmail.com>
2022-01-19 20:14:20 -0600

    initial commit of astro project from template

---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,19 @@
+import { h } from 'preact';
+import Styles from './styles.module.scss';
+
+function Nav() {
+	return (
+		<nav className={Styles.nav}>
+			<a className={Styles.logolink} href="/">
+				<div className={Styles.monogram}>AB</div>
+			</a>
+			<a className={Styles.link} href="/projects">
+				Portfolio
+			</a>
+			<a className={Styles.link} href="/about">
+				About
+			</a>
+		</nav>
+	);
+}
+export default Nav;
---
+++ src/pages/project/nested/lunar-eclipse.md
@@ -0,0 +1,14 @@
+---
+layout: ../../../layouts/project.astro
+title: Lunar Eclipse
+client: Self
+publishDate: 2020-03-04 00:00:00
+img: https://images.unsplash.com/photo-1548391350-1a529f6ea42d?fit=crop&w=1400&h=700&q=75
+description: |
+  Sample Project 2
+tags:
+  - design
+  - dev
+---
+
+Here's another sample project or something I'm going to write about.
