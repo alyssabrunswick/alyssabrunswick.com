7f8b345b88309342317a88f0dd32b8ae4d3b54d2
Alyssa Brunswick <alyssa.brunswick@gmail.com>
2022-01-19 20:50:32 -0600

    colors more me

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,11 +1,12 @@
 <span><slot /></span>
 
 <style>
-    span {
-        display: inline-block;
-        border: 3px solid currentColor;
-        padding: 0.5em 1em;
-        font-weight: 700;
-        text-transform: uppercase;
-    }
+  span {
+    display: inline-block;
+    border: 3px solid currentColor;
+    border-radius: 3px;
+    padding: 0.5em 1em;
+    font-weight: 700;
+    text-transform: uppercase;
+  }
 </style>
