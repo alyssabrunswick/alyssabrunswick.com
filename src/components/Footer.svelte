378684039050deb9fc1cd0cb9315d18bcdf1a26f
Alyssa Brunswick <alyssa.brunswick@gmail.com>
2022-02-17 20:13:28 -0600

    v importante content

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,7 @@
 <script>
   export let name = "Alyssa Brunswick";
+  // Yo! Yes, I built this with a template. Why build from scratch? Efficiency!
+  // https://github.com/withastro/astro/tree/main/examples/portfolio-svelte
 </script>
 
 <footer>
